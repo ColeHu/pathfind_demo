80f23431e148bbff8fb161a72e50cd7369f73b1c
ColeHu <a1624834559@gmail.com>
2021-11-06 18:52:08 +0800

    2021.11.6

--- src/initBack.js
+++
@@ -1,9 +0,0 @@
-function initBackground(renderer){
-    const loader = new THREE.TextureLoader();
-    let rt;
-    const texture = loader.load('../resource/skybox/tears_of_steel_bridge_2k.jpg', function (){
-        rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-        rt.fromEquirectangularTexture(renderer, texture);
-    })
-    return rt;
-}
